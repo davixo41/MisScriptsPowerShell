01d92f7e80c9f69b3095186d27014da6b38e986f
David Xequé <17017747+davixo41@users.noreply.github.com>
2022-10-04 11:32:53 -0500

    Add files via upload
    Added Find and copy files by Date Creation Script

---
+++ 01-FindByDataAndCopy.ps1
@@ -0,0 +1,7 @@
+﻿<#
+    REPLACE "PATH" WITH THE FOLDER WHERE FILES ARE LOCATED AND REPLACE "FILE_EX" WITH THE FILE EXTENSION
+    AND DESTINY_PATH WITH THE OUTPUT FOLDER WHEN FILES ARE LOCATED
+#>
+Get-ChildItem -Path "PATH" -Include "*.FILE_EX" -Recurse |
+Where-Object {$_.CreationTime -gt "dd/mm/aaaaa" -and $_.CreationTime -lt "dd/mm/aaaaa"} |
+Copy-Item -Destination "DESTINY_PATH"
